c889a092796d9d98488184a6062cd9a04dada75a
Sasha Markoff <Sasha.Snapp@equitybank.co.ke>
2022-09-27 12:28:44 +0300

    initial commit

---
+++ app/src/main/java/pet/markoff/eveus_charger_app/MainActivity.kt
@@ -0,0 +1,11 @@
+package pet.markoff.eveus_charger_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
